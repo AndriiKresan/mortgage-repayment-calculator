df3af1ff37363e7e9deca5762cdda6905f04b0de
Andrii Kresan <KresanAndriy@gmail.com>
2024-09-04 21:11:53 +0300

    added hover, active states

--- script.js
+++ script.js
@@ -1,76 +1,91 @@
 const amount = {
-  box: box_amount = document.querySelector(".amount-box"),
-  input: input_amount = document.querySelector(".input-amount"),
-  span: span_amount = document.querySelector(".amount-box span"),
-  error: error_amount = document.querySelector(".error-amount"),
-}
+  box: (box_amount = document.querySelector(".amount-box")),
+  input: (input_amount = document.querySelector(".input-amount")),
+  span: (span_amount = document.querySelector(".amount-box span")),
+  error: (error_amount = document.querySelector(".error-amount")),
+};
 
 const rate = {
   box: document.querySelector(".rate-box"),
   input: document.querySelector(".input-rate"),
   span: document.querySelector(".rate-box span"),
   error: document.querySelector(".error-rate"),
-}
+};
 
 const term = {
   box: document.querySelector(".term-box"),
   input: document.querySelector(".input-term"),
   span: document.querySelector(".term-box span"),
   error: document.querySelector(".error-term"),
-}
+};
 
-const box_type = document.querySelectorAll(".radio-box");
-const input_type = document.querySelectorAll("input[type=radio]");
-const error_type = document.querySelector(".error-type");
+const type = [
+  {
+    box: document.querySelector(".radio-box.repayment"),
+    input: document.querySelector(".radio-box.repayment input"),
+    error: document.querySelector(".error-type"),
+  },
+  {
+    box: document.querySelector(".radio-box.interest"),
+    input: document.querySelector(".radio-box.interest input"),
+  },
+];
 
 const button_calculate = document.querySelector(".calculate-button");
 
-amount['input'].addEventListener("focus", () => {
-  toggle_focus_style(amount['box']);
-  style_error(amount, false);
+[amount, term, rate].forEach((item) => {
+  item.input.addEventListener("focus", () => {
+    toggle_focus_style(item.box);
+    style_error(item, false);
+  });
 });
-amount['input'].addEventListener("blur", () => {
-  toggle_focus_style(amount['box']);
-});
-
-term['input'].addEventListener("focus", () => {
-  toggle_focus_style(term['box']);
-  style_error(term, false);
-});
-term['input'].addEventListener("blur", () => {
-  toggle_focus_style(term['box']);
+[amount, term, rate].forEach((item) => {
+  item.input.addEventListener("blur", () => {
+    toggle_focus_style(item.box);
+  });
 });
 
-rate['input'].addEventListener("focus", () => {
-  toggle_focus_style(rate['box']);
-  style_error(rate, false);
-});
-rate['input'].addEventListener("blur", () => {
-  toggle_focus_style(rate['box']);
+[amount, term, rate].forEach((item) => {
+  item.input.addEventListener("mouseover", () => {
+    hoverInputNumber(item.box, true);
+  });
 });
 
-input_type[0].addEventListener("click", () => {
-  box_type[0].classList.add("bc-dark-yellow");
-  box_type[1].classList.remove("bc-dark-yellow");
-  hide_element(error_type);
+[amount, term, rate].forEach((item) => {
+  item.input.addEventListener("mouseout", () => {
+    hoverInputNumber(item.box, false);
+  });
 });
 
-input_type[1].addEventListener("click", () => {
-  box_type[1].classList.add("bc-dark-yellow");
-  box_type[0].classList.remove("bc-dark-yellow");
-  hide_element(error_type);
+[amount, term, rate].forEach((item) => {
+  item.input.addEventListener("mouseout", () => {
+    hoverInputNumber(item.box, false);
+  });
 });
 
-// for (let i = 0; i <= input_type.length; i++) {
-//   input_type[i].addEventListener("focus", () => {
-//     boxe_type[i].classList.add("bc-dark-yellow");
-//   });
-// }
-// for (let i = 0; i <= input_type.length; i++) {
-//   input_type[i].addEventListener("blur", () => {
-//     boxe_type[i].classList.remove("bc-dark-yellow");
-//   });
-// }
+type[0].input.addEventListener("click", () => {
+  type[0].box.classList.add("bc-dark-yellow");
+  type[1].box.classList.remove("bc-dark-yellow");
+  hide_element(type[0].error);
+});
+
+type[1].input.addEventListener("click", () => {
+  type[1].box.classList.add("bc-dark-yellow");
+  type[0].box.classList.remove("bc-dark-yellow");
+  hide_element(type[0].error);
+});
+
+[type[0], type[1]].forEach((item) => {
+  item.box.addEventListener("mouseover", () => {
+    hoverInputRadio(item.box, true);
+  });
+});
+
+[type[0], type[1]].forEach((item) => {
+  item.box.addEventListener("mouseout", () => {
+    hoverInputRadio(item.box, false);
+  });
+});
 
 button_calculate.addEventListener("click", () => {
   validate_amount();
@@ -88,6 +103,15 @@ function show_element(element) {
   element.classList.add("visible");
 }
 
+function hoverInputNumber(element, state) {
+  if (state) element.classList.add("hover-input-number");
+  else element.classList.remove("hover-input-number");
+}
+function hoverInputRadio(element, state) {
+  if (state) element.classList.add("hover-input-radio");
+  else element.classList.remove("hover-input-radio");
+}
+
 function toggle_focus_style(element) {
   element.classList.toggle("focused-input");
 }
@@ -103,18 +127,18 @@ function input_empty(input) {
 
 function style_error(obj, state) {
   if (state) {
-    error_box(obj['box'], true);
-    obj['span'].classList.add("white-text");
-    show_element(obj['error']);
+    error_box(obj["box"], true);
+    obj["span"].classList.add("white-text");
+    show_element(obj["error"]);
   } else {
-    error_box(obj['box'], false);
-    obj['span'].classList.remove("white-text");
-    hide_element(obj['error']);
+    error_box(obj["box"], false);
+    obj["span"].classList.remove("white-text");
+    hide_element(obj["error"]);
   }
 }
 
 function validate_amount() {
-  if (input_empty(amount['input']) || amount['input'].value <= 0) {
+  if (input_empty(amount["input"]) || amount["input"].value <= 0) {
     style_error(amount, true);
     return false;
   }
@@ -122,7 +146,7 @@ function validate_amount() {
 }
 
 function validate_term() {
-  if (input_empty(term['input']) || term['input'].value <= 0) {
+  if (input_empty(term["input"]) || term["input"].value <= 0) {
     style_error(term, true);
     return false;
   }
@@ -130,7 +154,7 @@ function validate_term() {
 }
 
 function validate_rate() {
-  if (input_empty(rate['input']) || rate['input'].value <= 0) {
+  if (input_empty(rate["input"]) || rate["input"].value <= 0) {
     style_error(rate, true);
     return false;
   }
@@ -138,9 +162,8 @@ function validate_rate() {
 }
 
 function validate_type() {
-  if (input_type[0].checked || input_type[1].checked) {
+  if (type[0].input.checked || type[1].input.checked) {
     return true;
-  } else show_element(error_type);
+  } else show_element(type[0].error);
   return false;
 }
-
